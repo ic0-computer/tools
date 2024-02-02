02c53d6b51fc767564bd137faccda42d608ef8d0
bradyinfinity <bradyleecarrico@outlook.com>
2024-02-02 13:22:00 -0500

    principal generator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { Secp256k1KeyIdentity as S256 } from '@dfinity/identity-secp256k1';
+import { mnemonicToSeedSync, generateMnemonic } from 'bip39';
+import { HDKey } from 'ethereum-cryptography/hdkey.js';
 import { AccountIdentifier } from '@dfinity/ledger-icp';
 import { Principal } from '@dfinity/principal';
 
+// Create an SVG avatar from a principal or account id
 export function avatar(input: { principal: string } | { aid: string }): string {
   let account_id: string = '';
 
@@ -30,7 +34,20 @@ export function avatar(input: { principal: string } | { aid: string }): string {
   return svg;
 }
 
+// convert a principal to account id
 export function p2Aid(principal: Principal | string): string {
   const principalInstance = typeof principal === 'string' ? Principal.fromText(principal) : principal;
   return AccountIdentifier.fromPrincipal({ principal: principalInstance }).toHex();
 }
+
+// create a new principal and return the seed phrase
+export function generatePrincipal(seed_phrase?: string): {identity:Secp256k1KeyIdentity, seed_phrase:string} {
+  const mnemonic = seed_phrase ? seed_phrase : generateMnemonic()
+  const seed = mnemonicToSeedSync(mnemonic);
+  const privateKey = HDKey.fromMasterSeed(seed).derive("m/44'/223'/0'/0/0").privateKey as Uint8Array;
+  const identity = S256.fromSecretKey(privateKey);
+
+  return {identity: identity, seed_phrase: mnemonic};
+};
+
+export type Secp256k1KeyIdentity = S256;
